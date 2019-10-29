4bdffe536dd1306181019e82a496d5da2507d35d
hr-andrii <hr.andriygtr@gmail.com>
2019-10-29 20:04:03 +0000

    need to add teachers

--- js/slider.js
+++
@@ -1,6 +0,0 @@
-const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
